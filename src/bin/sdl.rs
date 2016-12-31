d5a71072ff0988ef2afe29f0bc5641ed081c177b
Arnau Siches <asiches@gmail.com>
2016-12-31 12:43:05 +0000

    Idea
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/bin/sdl.rs
+++ src/bin/sdl.rs
@@ -16,12 +16,15 @@ fn main() {
     // let mut world = World::random(n);
     // let mut world = World::infinite(n);
 
-    // let ppl = glider(glider(Population::empty(n), (0, 0)), (20, 10));
     let ppl = glider_br(
         glider_bl(
             Population::empty(n), (0, 7)
         ), (28, 0)
     );
+
+    // Idea:
+    // let ppl = Population::with([GliderBR(0, 7), GliderBL(28, 0)]);
+
     let world = World::new(ppl);
 
 
